c79afb5b8b7d9e8f6271b9c8045c4fc4aca210a7
Jiggly Puff <mian.wang.eit@gmail.com>
2017-01-03 19:49:40 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function dropList(){
+    document.getElementById('navContent').className -= "invisible"
+    document.getElementById('navContent').className += " animated"
+    document.getElementById('navContent').className += " bounceInDown"
+}
