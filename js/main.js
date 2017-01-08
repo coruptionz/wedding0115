869f0ffd372348ffcd39f0c53c115ba250a4fa56
Jiggly Puff <mian.wang.eit@gmail.com>
2017-01-08 11:15:55 -0800

    first commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ function dropList(){
     document.getElementById('navContent').className += " animated"
     document.getElementById('navContent').className += " bounceInDown"
 }
+
+$(".flower").velocity(
+  "transition.fadeIn", 
+              {stagger: 40, drag:true, duration:2000, delay:3000}
+ );
---
+++ photo/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/Kris-B/pen/dPxENe.
+
+ more info on http://nanogallery.brisbois.fr
