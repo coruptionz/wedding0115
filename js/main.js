e8a10aa2e5bcd2c66bc9b31471d229eb83f96cd0
Jiggly Puff <mian.wang.eit@gmail.com>
2017-01-15 14:21:29 -0800

    first commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,3 @@ function dropList(){
     document.getElementById('navContent').className += " bounceInDown"
 }
 
-$(".flower").velocity(
-  "transition.fadeIn", 
-              {stagger: 40, drag:true, duration:2000, delay:3000}
- );
